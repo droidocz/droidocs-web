293d06d0d4d520268aabaea9380ff72a2a5cddd2
Gabriel Dutra <nesk.frz@gmail.com>
2021-08-21 12:32:05 -0300

    Update site and github URL

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const config = {
   gatsby: {
     pathPrefix: '/',
-    siteUrl: 'https://hasura.io',
+    siteUrl: 'https://droidocsweb.gatsbyjs.io',
     gaTrackingId: null,
     trailingSlash: false,
   },
   header: {
     logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
-    logoLink: 'https://hasura.io/learn/',
+    logoLink: 'https://droidocsweb.gatsbyjs.io/',
     title:
       "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
-    githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
+    githubUrl: 'https://github.com/droidocz/droidocs-web',
     helpUrl: '',
     tweetText: '',
     social: `<li>
